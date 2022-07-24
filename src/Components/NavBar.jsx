376305d0dc68b2a66a73e1b3a8a028865322ddaa
dlwlrmla <joakomask@gmail.com>
2022-07-24 00:47:51 -0400

    algo de la pagina de inicio y weas

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -14,16 +14,16 @@ export const NavBar = () => {
     Instituto San Pablo Misionero
     </Navbar.Brand>
     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-    <Navbar.Collapse id="responsive-navbar-nav">
-    <Nav.Link style={{color:'white', marginRight:'15px'}} href="#features"><BsFillJournalBookmarkFill/> Colegio</Nav.Link>
-        <Nav.Link style={{color:'white', marginRight:'25px'}} href="#pricing"><AiFillCreditCard/> Pago mensualidad</Nav.Link>
-        <Nav.Link style={{color:'white', marginRight:'25px'}} href="#pricing"><BsFillPersonFill/> Padres</Nav.Link>
-        <Nav.Link style={{color:'white', marginRight:'25px'}} href="#pricing"><FaGraduationCap/> Alumnos</Nav.Link>
-        <Nav.Link style={{color:'white', marginRight:'25px'}} href="#pricing"><FaGraduationCap/> Pie</Nav.Link>
-        <Nav.Link style={{color:'white', marginRight:'25px'}} href="#pricing"><BiBrain/> Área psicológica</Nav.Link>
-        <Nav.Link style={{color:'white', marginRight:'25px'}}href="#pricing"><BsDownload/> Protocolos y Reglamentos</Nav.Link>
-        <Nav.Link style={{color:'white', marginRight:'25px'}} href="#pricing"><AiOutlineCheckSquare/> Admisión</Nav.Link>
-        <Nav.Link style={{color:'white', marginRight:'25px'}} href="#pricing"><BiNews/> Noticias</Nav.Link>    
+    <Navbar.Collapse id="responsive-navbar-nav" >
+    <Nav.Link style={{color:'white', marginRight:'15px'}} className='a' href="#features"><BsFillJournalBookmarkFill/> Colegio</Nav.Link>
+        <Nav.Link style={{color:'white', marginRight:'25px'}} className='a' href="#pricing"><AiFillCreditCard/> Pago mensualidad</Nav.Link>
+        <Nav.Link style={{color:'white', marginRight:'25px'}} className='a' href="#pricing"><BsFillPersonFill/> Padres</Nav.Link>
+        <Nav.Link style={{color:'white', marginRight:'25px'}} className='a' href="#pricing"><FaGraduationCap/> Alumnos</Nav.Link>
+        <Nav.Link style={{color:'white', marginRight:'25px'}} className='a' href="#pricing"><FaGraduationCap/> Pie</Nav.Link>
+        <Nav.Link style={{color:'white', marginRight:'25px'}} className='a' href="#pricing"><BiBrain/> Área psicológica</Nav.Link>
+        <Nav.Link style={{color:'white', marginRight:'25px'}} className='a' href="#pricing"><BsDownload/> Protocolos y Reglamentos</Nav.Link>
+        <Nav.Link style={{color:'white', marginRight:'25px'}} className='a' href="#pricing"><AiOutlineCheckSquare/> Admisión</Nav.Link>
+        <Nav.Link style={{color:'white', marginRight:'25px'}} className='a' href="#pricing"><BiNews/> Noticias</Nav.Link>    
         
         
     </Navbar.Collapse>
