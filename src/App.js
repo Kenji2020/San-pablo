b1f081607ed33877ced2a3e996237bf748f26db6
Kenji2020 <83725041+Kenji2020@users.noreply.github.com>
2022-07-23 23:23:02 -0400

    segundo push

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import { NavBar } from './Components/NavBar';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import  {Footer}  from './Components/Footer';
 function App() {
   return (
+    <>
     <NavBar/>
+    <div className="container">
+      <div className="row">
+        <div className="col-md-12">
+          <h1>Hello World</h1>
+        </div>
+      </div>
+    </div>
+    
+    <Footer />
+    </>
   );
 }
 
